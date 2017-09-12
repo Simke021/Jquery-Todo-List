f74aee2225a66266ae65f08b31da2774f3277c6e
Aleksandar Ljubišić <simke20@yahoo.com>
2017-09-12 15:42:15 +0200

    Add event listener on span, li and body

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event){
+    alert("click on span");
+});
+
+$("ul").click(function(){
+    alert("click on ul");
+});
+
+$("body").click(function(){
+    alert("click on body");
+});
