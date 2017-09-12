6d278da5d98a18105bd9594b443b2790338cc1e4
Aleksandar Ljubišić <simke20@yahoo.com>
2017-09-12 16:21:12 +0200

    Creating new Todo after press enter

--- js/main.js
+++ js/main.js
@@ -3,10 +3,26 @@ $("li").click(function(){
     $(this).toggleClass("completed");
 });
 
-// Click on X to delete Todo
+// Brisem Todo klikom na X
 $("span").click(function(event){
      $(this).parent().fadeOut(500, function(){
         $(this).remove();
      });
     event.stopPropagation();
+});
+
+// Selektujem input polje
+$("input[type='text']").keypress(function(){
+
+    // Proveravam da li je pritisnuto enter na tastaturi
+    if(event.which === 13){
+
+        // Uzimam iz input-a sta je korisnik upisao
+       var todoText = $(this).val();
+
+       $(this).val = "";
+       // Kreiram novi li i dodajem u ul
+       $("ul").append("<li><span>X</span> "+ todoText +"</li>");
+
+    }
 });
